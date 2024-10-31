56c286b6042c02aba6c0ef83268396188aafc8b6
suraj sakhare <sakharesuraj10@gmail.com>
2024-10-31 17:36:11 +0530

    update readme genrator

--- src/components/badge/Badge.jsx
+++ src/components/badge/Badge.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function Badge({ key,BadgeName, className , onClick}) {
+function Badge({ BadgeName, className , onClick}) {
   return (
-    <div key={key}>
+    <div >
       <span
         onClick={onClick}
         className={`text-sm font-bold me-2 px-3 py-1.5 cursor-pointer bg-green-300 text-green-900 rounded ${className}`}
--- src/pages/ReadmeGenerator.jsx
+++ src/pages/ReadmeGenerator.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MarkdownIt from 'markdown-it';
 import markdownItTaskLists from 'markdown-it-task-lists';
 import 'prismjs/themes/prism-tomorrow.css';
@@ -8,9 +8,7 @@ import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-http';
 import { Clipboard, Trash } from 'lucide-react';
 import Badge from '../components/badge/Badge';
-import badgesData from '../data.json'
-
-
+import badgesData from '../data.json';
 
 const ReadmeGenerator = () => {
   const [markdownText, setMarkdownText] = useState(`
@@ -36,99 +34,88 @@ const ReadmeGenerator = () => {
 GET /api/data
 Host: example.com
 \`\`\`
-
-`);
+  `);
 
   const [view, setView] = useState('preview');
   const md = new MarkdownIt().use(markdownItTaskLists);
+
+  useEffect(() => {
+    Prism.highlightAll();
+  }, [markdownText]);
 
   const handleChange = (event) => {
     setMarkdownText(event.target.value);
   };
 
   const renderedMarkdown = md.render(markdownText);
-  setTimeout(() => Prism.highlightAll(), 0);
 
   const copyToClipboard = () => {
-    navigator.clipboard.writeText(renderedMarkdown);
+    navigator.clipboard.writeText(markdownText);
     alert('Copied to clipboard!');
   };
 
   const clearText = () => {
     setMarkdownText('');
   };
-  const addgitprofile=()=>{
-    setMarkdownText(`
-I am a bachelor's student at {} . I am a {} from India with a proven ability to deliver high-quality solutions. Currently working on My Projects.
 
-- 🔭 I’m currently working on **{on project}**
-
-- 🌱 I’m currently learning ** this task **
-
-- 👨‍💻 All of my projects are available at My Portfolio website -- {add your link}
-`)
-  }
   const handleBadgeClick = async (filename) => {
     try {
-      const response = await fetch(`public/${filename}`); // Fetch the markdown file
+      const response = await fetch(`/templates/${filename}`); 
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
       const text = await response.text(); 
-      setMarkdownText(text); 
+      setMarkdownText(text);
     } catch (error) {
       console.error('Error fetching the markdown file:', error);
     }
   };
+
   return (
     <div className="w-full px-4 md:px-0 ">
       <div className="md:w-[66rem] mx-auto mt-20 mb-4">
         <h1 className="tagline text-5xl text-center font-semibold">
-        Start Editing Your <span className='text-[#10B981]'>README.md</span> Now</h1>
+          Start Editing Your <span className='text-[#10B981]'>README.md</span> Now
+        </h1>
         <div className='w-full py-4'>
-        <h2 className='text-xl font-semibold mb-2'>Templates For You : </h2>
-        <div className='flex gap-2 flex-wrap justify-center'>
-        {badgesData?.map(badge => (
-          <Badge
-          key={badge.id}
-          BadgeName={badge.name}
-          className={badge.className}
-          onClick={() => handleBadgeClick(badge.filename)} // Pass the click handler
-        />
-        ))}
+          <h2 className='text-xl font-semibold mb-2'>Templates For You : </h2>
+          <div className='flex gap-2 flex-wrap justify-center'>
+            {badgesData?.map(badge => (
+              <Badge
+                key={badge.id}
+                BadgeName={badge.name}
+                className={badge.className}
+                onClick={() => handleBadgeClick(badge.filename)} 
+              />
+            ))}
+          </div>
         </div>
-        </div>
-        <div className="flex flex-col gap-4 md:flex-row w-full pt-4 h-[44rem] md:h-auto ">
+        <div className="flex flex-col gap-4 md:flex-row w-full pt-4 md:h-auto ">
           <div className="flex flex-col gap-2 md:w-1/2 h-1/2 md:h-full">
-            <div className="flex gap-4 justify-end  pb-2  ">
-              
-             <div>
-             <button onClick={copyToClipboard} className="icon-button text-green-400 font-semibold">
+            <div className="flex gap-4 justify-end pb-2">
+              <button onClick={copyToClipboard} className="icon-button text-green-400 font-semibold">
                 <Clipboard size={20} />
               </button>
               <button onClick={clearText} className="icon-button text-red-400 font-semibold">
                 <Trash size={20} />
               </button>
-             </div>
             </div>
-            <div className='w-full md:h-[44rem] flex-grow '>
             <textarea
-              className="textarea border rounded-md p-4 h-full w-full flex-shrink-0"
+              className="textarea border rounded-md p-4 !h-[32rem]  w-full flex-shrink-0"
               value={markdownText}
               onChange={handleChange}
             />
-            </div>
           </div>
-          <div className=" md:w-1/2 flex flex-col h-1/2 md:h-full">
-            <div className="flex gap-2 justify-end options pb-3 ">
+          <div className="md:w-1/2 flex flex-col h-1/2 md:h-full">
+            <div className="flex gap-2 justify-end options pb-3">
               <button onClick={() => setView('preview')} className={view === 'preview' ? 'active' : ''}>
                 Preview
               </button>
-              <button onClick={() => setView('raw')} className={view === 'raw' ? 'active' : '' } >
+              <button onClick={() => setView('raw')} className={view === 'raw' ? 'active' : ''}>
                 Raw
               </button>
             </div>
-            <div className="preview border p-4 rounded-lg h-full  ">
+            <div className="preview border p-4 rounded-lg h-full">
               {view === 'preview' ? (
                 <div dangerouslySetInnerHTML={{ __html: renderedMarkdown }} className='h-full overflow-y-scroll' />
               ) : (
