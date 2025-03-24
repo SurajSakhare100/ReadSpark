5188877870761a6f3772e78c6f004af9b272f3e2
SurajSakhare100 <sakharesuraj10@gmail.com>
2025-03-24 21:54:08 +0530

    next-auth updated

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,53 +1,5 @@
 import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
-import { NextAuthOptions } from 'next-auth';
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-      authorization: {
-        params: {
-          scope: 'read:user repo'
-        }
-      },
-      profile(profile) {
-        return {
-          id: profile.id.toString(),
-          name: profile.name || profile.login,
-          email: profile.email,
-          image: profile.avatar_url,
-          username: profile.login,
-        };
-      },
-    }),
-  ],
-  pages: {
-    signIn: '/auth/signin',
-    error: '/auth/error',
-  },
-  callbacks: {
-    async jwt({ token, user, account }) {
-      if (account && account.access_token) {
-        token.accessToken = account.access_token;
-      }
-      if (user) {
-        token.id = user.id;
-        token.username = (user as any).username;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if (session.user) {
-        session.user.id = token.id as string;
-        session.user.username = token.username as string;
-        session.user.accessToken = token.accessToken as string;
-      }
-      return session;
-    },
-  },
-};
+import { authOptions } from './config';
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
