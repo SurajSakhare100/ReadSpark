fb57373946631d06db004a217ab61fe73d5794b7
SurajSakhare100 <sakharesuraj10@gmail.com>
2025-03-30 10:59:49 +0530

    params issue resolve

--- src/app/api/documents/[id]/route.ts
+++ src/app/api/documents/[id]/route.ts
@@ -8,10 +8,10 @@ import User from '@/models/User';
 // GET method to fetch a document by ID
 export async function GET(
   req: NextRequest,
-  {params} : { params: { id: string } }
+  params : { params: { id: string } }
 ) {
   try {
-    const id = params.id;
+    const id = params.params.id;
     const session = await getServerSession(authOptions);
 
     if (!session?.user) {
