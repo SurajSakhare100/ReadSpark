3b71e5b61db902d803537d70b9d623e390d5c2f6
SurajSakhare100 <sakharesuraj10@gmail.com>
2025-03-30 13:55:35 +0530

    adjust css container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
         {/* Viewport Meta Tag */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body>
+      <body >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
